1c5b395a849fa7dabc57c6cb7a2792c130b7d676
VVMachine <vik.atamanenko@gmail.com>
2021-03-14 00:07:30 +0200

    add baseMarckup and prices JSON file

---
+++ src/components/PriceItem/PriceItem.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import styles from './PriceItem.module.css';
+
+const PriceItem = () => {
+    return <div className={styles.container}>
+        
+    </div>
+}
+
+export default PriceItem;
