a078bd2fac886dc9a7db92f2e30e4be13b0450fd
VVMachine <vik.atamanenko@gmail.com>
2021-03-14 20:25:37 +0200

    add styles into Slider container

--- src/components/PriceItem/PriceItem.js
+++ src/components/PriceItem/PriceItem.js
@@ -1,9 +1,9 @@
 import React from 'react';
 import styles from './PriceItem.module.css';
 
-const PriceItem = () => {
+const PriceItem = ({text}) => {
     return <div className={styles.container}>
-        
+        {text}
     </div>
 }
 
