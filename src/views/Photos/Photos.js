fcc5afd3d3823fabfc23ad4a1c3017afd0333a41
Oleksandr Zdebskyi <zdebskyi@MacBook-Air-Oleksandr.local>
2021-03-25 10:23:33 +0200

    added tabs to photos view

--- src/views/Photos/Photos.js
+++ src/views/Photos/Photos.js
@@ -1,7 +1,48 @@
 import React from "react";
+import Layout from "../../components/Layout/Layout";
+import { Tab, Tabs, TabList, TabPanel } from "react-tabs";
+import "react-tabs/style/react-tabs.css";
+import styles from "./Photos.module.css";
 
 const Photos = () => {
-  return <div>Photos</div>;
+  return (
+    <>
+      <section className={styles.container}>
+        <h2 className={styles.photosTitle}>Фото клубів</h2>
+        <Layout>
+          <Tabs>
+            <TabList>
+              <Tab>Ірпінська 76</Tab>
+              <Tab>Семашко 13</Tab>
+              <Tab>Кольцова 14</Tab>
+              <Tab>Семашко 8а</Tab>
+            </TabList>
+
+            <TabPanel>
+              <div className={styles.photosContainer}>
+                <h2>Any content 1</h2>
+              </div>
+            </TabPanel>
+            <TabPanel>
+              <div className={styles.photosContainer}>
+                <h2>Any content 2</h2>
+              </div>
+            </TabPanel>
+            <TabPanel>
+              <div className={styles.photosContainer}>
+                <h2>Any content 3</h2>
+              </div>
+            </TabPanel>
+            <TabPanel>
+              <div className={styles.photosContainer}>
+                <h2>Any content 4</h2>
+              </div>
+            </TabPanel>
+          </Tabs>
+        </Layout>
+      </section>
+    </>
+  );
 };
 
 export default Photos;
